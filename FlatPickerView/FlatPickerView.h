8505daab7129bf2365b66290917ab59f4ead22e0
Luciano Almeida <passos.luciano@outlook.com>
2016-12-26 17:14:09 -0200

    Initial Commit

---
+++ FlatPickerView/FlatPickerView.h
@@ -0,0 +1,19 @@
+//
+//  FlatPickerView.h
+//  FlatPickerView
+//
+//  Created by Luciano Almeida on 26/12/16.
+//  Copyright © 2016 Luciano Almeida. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FlatPickerView.
+FOUNDATION_EXPORT double FlatPickerViewVersionNumber;
+
+//! Project version string for FlatPickerView.
+FOUNDATION_EXPORT const unsigned char FlatPickerViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FlatPickerView/PublicHeader.h>
+
+
